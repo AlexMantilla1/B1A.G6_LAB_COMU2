fab75be673cf1ffdfaecd3ef41770c6bd3b57d91
AlexMantilla1 <alexmantilla97@gmail.com>
2019-10-18 09:21:36 -0500

    Solved to 7.2, commit for stetic proposes

--- Lab4/lab4_2_v2.py
+++ Lab4/lab4_2_v2.py
@@ -2,8 +2,7 @@
 import numpy as np
 import math
 from matplotlib import pyplot as plt
-
-# Parametros de la senal analiada
+#Parametros de la senal analiada
 f = 3540.
 Fsamp = 9000. # la frecuencia de muestreo
 # La senal discreta
@@ -13,11 +12,15 @@ t = n/Fsamp
 signal = np.cos(2.*math.pi*f*t)
 fourier = np.fft.fft(signal)
 fourier_mejor = np.fft.fftshift(fourier)
+# Calculo la fft al cuadrado
+fourier_cuadrado = np.power(fourier_mejor,2)
 # Calculos para relacional la senal discreta con el mundo real
 Fmin = -Fsamp/2.
 Fresol = Fsamp/N
 Fmax = -Fmin-Fresol
 f = np.linspace(Fmin,Fmax,N)
 # Grafica
-plt.stem(f,np.absolute(fourier_mejor))
+plt.stem(f,np.absolute(fourier_cuadrado))
+plt.title('|signal(f)| ^ 2')
+plt.xlabel('Frequency [Hz]')
 plt.show()
