83631693dae944a17cf8fa7342acfe2bdaab314e
AlexMantilla1 <alexmantilla97@gmail.com>
2020-01-17 10:46:06 -0500

    Comming back from vacations, Lab4 was not finished but delivered, Starting Lab5

--- Lab4/Library_GNU.py
+++ Lab4/Library_GNU.py
@@ -67,12 +67,12 @@ class e_add_cc(gr.sync_block):
         return len(out0)
 
 ####################################################
-##     clase e_add_ff                             ##
+##     clase e_add_ff3                             ##
 ####################################################
-class e_add_ff(gr.sync_block):
+class e_add_ff3(gr.sync_block):
     """consiste en un bloque para una suma escalada de dos senales reales. Por lo tanto hay dos senales de entrada y una de salida. Si escala=0.5 lo que se logra es promediar las dos senales"""
 
-    def __init__(self, escala=0.5):
+    def __init__(self, escala = 0.5):
 
         gr.sync_block.__init__(
             self,
@@ -90,6 +90,28 @@ class e_add_ff(gr.sync_block):
         return len(out0)
 
 ####################################################
+##     clase e_add_ff                             ##
+####################################################
+class e_add_ff(gr.sync_block):
+    """consiste en un bloque para una suma escalada de dos senales reales. Por lo tanto hay dos senales de entrada y una de salida. Si escala=0.5 lo que se logra es promediar las dos senales"""
+
+    def __init__(self, escala = 0.5):
+
+        gr.sync_block.__init__(
+            self,
+            name='e_add_ff',
+            in_sig=[np.float32,np.float32],
+            out_sig=[np.float32]
+        )
+        self.escala=escala
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        in1 = input_items[1]
+        out0 = output_items[0]
+        out0[:]=(in0+in1)*self.escala
+        return len(out0)
+
+####################################################
 ##     clase e_vector_fft_ff                      ##
 ####################################################
 # En color rojo aparece aquello que hace que el bloque sea vectorial
@@ -101,13 +123,13 @@ class e_vector_fft_ff(gr.sync_block):
         gr.sync_block.__init__(
             self,
             name='e_vector_fft_ff',
-            in_sig=[(np.float32)],
-            out_sig=[(np.float32)]
+            in_sig = [(np.float32,N)],
+            out_sig = [(np.float32,N)]
         )
         self.N = N
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out0 = output_items[0]
-        out0[:]=abs(np.fft.fftshift(np.fft.fft(in0,self.N),1))
+        out0[:] = abs(np.fft.fftshift(np.fft.fft(in0,self.N),1))
         return len(output_items[0])
--- Lab4/lab4_4.3_v2.py
+++ Lab4/lab4_4.3_v2.py
@@ -38,39 +38,53 @@ class flujograma(gr.top_block):
 
         # Las variables usadas en el flujograma
         samp_rate = 32000
-        f = 1000
-        N = 128
+        f = 4500
+        N = 512
         # Los bloques
-        src = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, f, 1, 0)
-        nse = analog.noise_source_f(analog.GR_GAUSSIAN, 0.1)
-        add = misbloques.e_add_ff(1.0)
-        snk = qtgui.time_sink_f(
-            512, # numero de muestras en la ventana del osciloscopio
+        self.src = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, f, 1, 0)
+        self.src2 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 2*f, 1, 0)
+        self.nse = analog.noise_source_f(analog.GR_GAUSSIAN, 0.1)
+        self.nse2 = analog.noise_source_f(analog.GR_GAUSSIAN, 0.12)
+        self.add = misbloques.e_add_ff(1.0)
+        self.add2 = misbloques.e_add_ff(1.0)
+        self.snk = qtgui.time_sink_f(
+            N, # numero de muestras en la ventana del osciloscopio
             samp_rate,
-            "senal promediada", # nombre que aparece en la grafica
-            1 # Nuemero de entradas del osciloscopio
+            "senal con Ruido", # nombre que aparece en la grafica
+            2 # Nuemero de entradas del osciloscopio
         )
-        str2vec=blocks.stream_to_vector(gr.sizeof_float*1, N)
-        e_fft=misbloques.e_vector_fft_ff(N)
-        vsnk = qtgui.vector_sink_f(
+        self.str2vec = blocks.stream_to_vector(gr.sizeof_float*1, N)
+        self.str2vec2 = blocks.stream_to_vector(gr.sizeof_float*1, N)
+        self.e_fft = misbloques.e_vector_fft_ff(N)
+        self.e_fft2 = misbloques.e_vector_fft_ff(N)
+        self.vsnk = qtgui.vector_sink_f(
             N,
             -samp_rate/2.,
             samp_rate/N,
             "Frecuencia [Hz]",
             "Magnitud",
             "FT en Magnitud",
-            1 # Number of inputs
+            2 # Number of inputs
         )
-        vsnk.enable_autoscale(True)
+        self.vsnk.enable_autoscale(True)
         # Las conexiones
-        self.connect(src, (add, 0))
-        self.connect(nse, (add, 1))
-        self.connect(add, snk)
-        self.connect(add, str2vec, e_fft, vsnk)
+        self.connect(self.src, (self.add, 0))
+        self.connect(self.nse, (self.add, 1))
+        self.connect(self.src2, (self.add2, 0))
+        self.connect(self.nse2, (self.add2, 1))
+        self.connect(self.add, (self.snk,0))
+        self.connect(self.add, self.str2vec)
+        self.connect(self.add2, (self.snk,1))
+        self.connect(self.add2, self.str2vec2)
+        self.connect(self.str2vec, self.e_fft)
+        self.connect(self.str2vec2, self.e_fft2)
+        self.connect(self.e_fft, (self.vsnk,0))
+        self.connect(self.e_fft2, (self.vsnk,1))
 
         # La configuracion para graficar
-        pyobj = sip.wrapinstance(vsnk.pyqwidget(), Qt.QWidget)
-        pyobj.show()
+        self.pyobj = sip.wrapinstance(self.vsnk.pyqwidget(), Qt.QWidget)
+        #self.pyobj = sip.wrapinstance(self.snk.pyqwidget(), Qt.QWidget)
+        self.pyobj.show()
 
 ###########################################################
 ###                LA CLASE PRINCIPAL                   ###
